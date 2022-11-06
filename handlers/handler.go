850504673e9d31d4a0dbf825605f420d78dc0b98
Toskosz <thiago.tokarski2001@gmail.com>
2022-11-06 03:39:55 -0300

    add unhandledmethod api response

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "github.com/Toskosz/everythingreviewed/models"
+import (
+	"net/http"
+
+	"github.com/Toskosz/everythingreviewed/models"
+	"github.com/Toskosz/everythingreviewed/models/api_error"
+	"github.com/Toskosz/everythingreviewed/services"
+	"github.com/aws/aws-lambda-go/events"
+)
 
 type Handler struct {
 	userService models.InterfaceUserService
@@ -10,4 +17,8 @@ func NewHandler(user models.InterfaceUserService) Handler {
 	return Handler{
 		userService: user,
 	}
+}
+
+func (h *Handler) UnhandledMethod() (*events.APIGatewayProxyResponse, error) {
+	return services.ApiResponse(http.StatusMethodNotAllowed, api_error.NewServiceUnavailable())
 }
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/Toskosz/everythingreviewed/db"
+	"github.com/Toskosz/everythingreviewed/handlers"
+	"github.com/Toskosz/everythingreviewed/services"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(req events.APIGatewayProxyRequest) (
+	*events.APIGatewayProxyResponse, error) {
+
+	dbConn := db.NewDataBaseConn("user")
+	userService := services.NewUserService(dbConn)
+	h := handlers.NewHandler(userService)
+
+	if req.HTTPMethod == "POST" && req.Path == "/register" {
+		return h.Register(req)
+	} else if req.HTTPMethod == "POST" && req.Path == "/login" {
+		return h.Login(req)
+	} else if req.HTTPMethod == "POST" && req.Path == "/logout" {
+		return h.Logout(req)
+	} else {
+		return h.UnhandledMethod()
+	}
+}
+
+func main() {
+	lambda.Start(handler)
+}
