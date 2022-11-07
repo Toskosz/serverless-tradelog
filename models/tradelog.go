0b5492bde6abd694b21094827c632e1a7be4f6e2
thiago <thiago.tokarski2001@gmail.com>
2022-11-07 18:28:56 -0300

    fix types in tradelog struct

--- models/tradelog.go
+++ models/tradelog.go
@@ -1,9 +1,5 @@
 package models
 
-import "time"
-
-type Timestamp time.Time
-
 type InterfaceDBLog interface {
 	GetLogById(id int) (*TradeLog, error)
 	GetLogsByUserId(userId int) ([]*TradeLog, error)
@@ -21,17 +17,17 @@ type InterfaceLogService interface {
 }
 
 type TradeLog struct {
-	Id                  uint      `json:"id"`
-	Userid              string    `json:"user-id" gorm:"unique"`
-	Ativo               string    `json:"ativo"`
-	Resultado           string    `json:"resultado"`
-	Contratos           int       `json:"contratos"`
-	MEP                 string    `json:"mep"`
-	MEN                 string    `json:"men"`
-	TempoOperacao       float64   `json:"duracao"`
-	PrecoCompra         float32   `json:"preco-compra"`
-	PrecoVenda          float32   `json:"preco-venda"`
-	TimestampAbertura   Timestamp `json:"hora-abertura"`
-	TimestampFechamento Timestamp `json:"hora-fechamento"`
-	Desc                string    `json:"descricao"`
+	Id                  uint    `json:"id"`
+	Userid              string  `json:"user-id" gorm:"unique"`
+	Ativo               string  `json:"ativo"`
+	Resultado           string  `json:"resultado"`
+	Contratos           int     `json:"contratos"`
+	MEP                 string  `json:"mep"`
+	MEN                 string  `json:"men"`
+	TempoOperacao       int     `json:"duracao"`
+	PrecoCompra         float32 `json:"preco-compra"`
+	PrecoVenda          float32 `json:"preco-venda"`
+	TimestampAbertura   string  `json:"hora-abertura"`
+	TimestampFechamento string  `json:"hora-fechamento"`
+	Desc                string  `json:"descricao"`
 }
