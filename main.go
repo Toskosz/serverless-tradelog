f9518098cfa3f102605c796bf577a861a10eba61
thiago <thiago.tokarski2001@gmail.com>
2022-11-10 19:13:54 -0300

    added new endpoints to the main.go

--- main.go
+++ main.go
@@ -11,12 +11,22 @@ func handler(req events.APIGatewayProxyRequest) (
 
 	h := handlers.NewHandler()
 
-	if req.HTTPMethod == "POST" && req.Path == "/register" {
+	if req.HTTPMethod == "POST" && req.Resource == "/register" {
 		return h.Register(req)
-	} else if req.HTTPMethod == "POST" && req.Path == "/login" {
+	} else if req.HTTPMethod == "POST" && req.Resource == "/login" {
 		return h.Login(req)
-	} else if req.HTTPMethod == "POST" && req.Path == "/logout" {
+	} else if req.HTTPMethod == "POST" && req.Resource == "/logout" {
 		return h.Logout(req)
+	} else if req.HTTPMethod == "GET" && req.Resource == "/my/logs/{log-ts}" {
+		return h.GetLog(req)
+	} else if req.HTTPMethod == "GET" && req.Resource == "/my/logs" {
+		return h.GetMyLogs(req)
+	} else if req.HTTPMethod == "POST" && req.Resource == "/my/logs" {
+		return h.CreateLog(req)
+	} else if req.HTTPMethod == "PUT" && req.Resource == "/my/logs" {
+		return h.UpdateLog(req)
+	} else if req.HTTPMethod == "DELETE" && req.Resource == "/my/logs/{log-ts}" {
+		return h.DeleteLog(req)
 	} else {
 		return h.UnhandledMethod()
 	}
