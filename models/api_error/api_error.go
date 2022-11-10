097f12eb8acd356c66efe7f2e5bd1e2ee7371268
thiago <thiago.tokarski2001@gmail.com>
2022-11-10 20:24:05 -0300

    add some validations to the services

--- models/api_error/api_error.go
+++ models/api_error/api_error.go
@@ -5,4 +5,6 @@ const (
 	UnauthorizedError       = "Unauthorized"
 	InvalidCredentialsError = "Invalid user or password"
 	DuplicateEmailError     = "This email is taken"
+	InvalidDateTime         = "Date-time must be 'YYYY-MM-DDTHH:MM:SSZ'"
+	InvalidMepMen           = "Invalid MEP or MEN value"
 )
