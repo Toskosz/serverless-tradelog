eaedb6cc19e83d956108caeb00d8e2b4d9bd5a74
Toskosz <thiago.tokarski2001@gmail.com>
2022-11-16 12:29:42 -0300

    fix error name and value

--- models/api_error/api_error.go
+++ models/api_error/api_error.go
@@ -4,7 +4,7 @@ const (
 	InternalError           = "Internal error. Try again"
 	UnauthorizedError       = "Unauthorized"
 	InvalidCredentialsError = "Invalid user or password"
-	DuplicateEmailError     = "This email is taken"
+	DuplicateUsernameError  = "This username is taken"
 	InvalidDateTime         = "Date-time must be 'YYYY-MM-DDTHH:MM:SSZ'"
 	InvalidMepMen           = "Invalid MEP or MEN value"
 )
